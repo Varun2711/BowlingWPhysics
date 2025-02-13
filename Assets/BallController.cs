1d7377a8f26bd512e35bd98da1b6a7599364ef62
Varun Patel <rvjpatel99@gmail.com>
2025-02-12 22:20:55 -0800

    basic ball setup and movement also pins

---
+++ Assets/BallController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+    private bool isBallLaunched;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // grab reference to rigidbody
+        ballRB = GetComponent<Rigidbody>();
+
+        // add listener to the onSpacedPressed event
+        inputManager.onSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        // check if ball has already been launched
+        // if so, then return immediately without adding force
+        if (isBallLaunched)
+        {
+            return;
+        }
+
+        // activate the isBallLaunched flag
+        isBallLaunched = true;
+
+        // add force to the ball i.e launch the ball
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+}
---
+++ Assets/InputManager.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    public UnityEvent onSpacePressed = new UnityEvent();
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onSpacePressed?.Invoke();
+        }
+    }
+}
